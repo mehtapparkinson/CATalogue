5a9d7d946b06e5c91b3b5cb8efc54fdaf33214d3
Mehtap <97293905+mehtapparkinson@users.noreply.github.com>
2024-09-19 11:14:50 +0200

    Initial commit

---
+++ src/main/java/es/mehtap/CATalogue/CaTalogueApplication.java
@@ -0,0 +1,13 @@
+package es.mehtap.CATalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaTalogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaTalogueApplication.class, args);
+	}
+
+}
