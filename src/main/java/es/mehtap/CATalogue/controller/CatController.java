960c35db8146b5c9b68e6fcd00946c759fa4017a
Mehtap <97293905+mehtapparkinson@users.noreply.github.com>
2024-09-20 20:32:36 +0200

    delete a cat added

--- src/main/java/es/mehtap/CATalogue/controller/CatController.java
+++ src/main/java/es/mehtap/CATalogue/controller/CatController.java
@@ -4,20 +4,20 @@ package es.mehtap.CATalogue.controller;
 import es.mehtap.CATalogue.model.Cat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.*;
 import es.mehtap.CATalogue.repository.CatRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/cats")
 public class CatController {
-
     //CREATE LOGGER
     private static final Logger logger = LoggerFactory.getLogger(CatController.class);
-
     //INFO
     @Value("${adoption.center.name}")
     private String centerName;
@@ -49,13 +49,25 @@ public class CatController {
         logger.info("Adding a new cat: {}", cat.getName());
         return catRepository.save(cat);
     }
-
+    //GET SPECIFIC CAT
     @GetMapping("/{id}")
     public Cat getCatById(@PathVariable int id) {
         return catRepository.findById(id).orElseThrow(() -> {
             logger.error("Cat with id {} not found", id);
             return new RuntimeException("Cat not found");
         });
+    }
+
+    //DELETE A CAT
+    @DeleteMapping("/{id}")
+    public void deleteCat(@PathVariable int id) {
+        if (catRepository.existsById(id)) {
+            logger.info("Deleting cat with id {}", id);
+            catRepository.deleteById(id);
+        } else {
+            logger.error("Cat with id {} not found", id);
+            throw new ResponseStatusException(HttpStatusCode.valueOf(404), "Cat not found");
+        }
     }
 
 
